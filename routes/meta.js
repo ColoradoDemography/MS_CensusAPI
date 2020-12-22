16b9e515285727c9312bb7fca7dd696b9a71266e
TBleess <todd.bleess@gmail.com>
2020-12-22 09:04:28 -0700

    Meta to 1519

--- routes/meta.js
+++ routes/meta.js
@@ -11,7 +11,7 @@ module.exports = function(app, pg, conString){
 app.get('/meta', function(req, res) {
 
   //potential multi select (comma delimited list)
-  var db = req.query.db || "acs1317";
+  var db = req.query.db || "acs1519";
   var schema = req.query.schema || "data";  
 
   //Query metadata
