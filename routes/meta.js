55e3a34a366cbcdbbb23524269e4298d2e156de2
TBleess <todd.bleess@state.co.us>
2026-02-02 15:42:01 -0700

    Update default database from 'acs1923' to 'acs2024'

--- routes/meta.js
+++ routes/meta.js
@@ -11,7 +11,7 @@ module.exports = function(app, pg, conString){
 app.get('/meta', function(req, res) {
 
   //potential multi select (comma delimited list)
-  var db = req.query.db || "acs1923";
+  var db = req.query.db || "acs2024";
   var schema = req.query.schema || "data";  
 
   //Query metadata
