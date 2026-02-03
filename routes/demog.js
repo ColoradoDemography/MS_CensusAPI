05aa569b09a69964119e39a24c357f060e553e64
TBleess <todd.bleess@state.co.us>
2026-02-03 09:45:50 -0700

    Change default db from 'acs2024' to 'acs1418'

--- routes/demog.js
+++ routes/demog.js
@@ -383,7 +383,7 @@ function array_unshift(array) {
     
  //potential single select
 var type = req.query.type || 'json';
-var db = req.query.db || 'acs2024';
+var db = req.query.db || 'acs1418';
 //set default for schema if it is missing
 var schema = req.query.schema ||  function(){
   if(db==='acs2024' || db==='acs1923' || db==='acs1822' || db==='acs1721' || db==='acs1620' || db==='acs1519' || db==='acs1418' || db==='acs1317' || db==='acs1216' || db==='acs1115' || db==='acs1014' || db==='acs0913' || db==='acs0812' || db==='acs0711' || db==='acs0610' || db==='c2010' || db==='c2020'){return 'data';}
